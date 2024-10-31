8d39d7e336171591b3cf87ade0592e529be56c28
HarshRohilla1 <harshrohilla199@gmail.com>
2024-10-31 08:50:31 +0530

    Home Screen

--- app/src/main/java/com/example/pglocator/Screens/HomeScreen.kt
+++ app/src/main/java/com/example/pglocator/Screens/HomeScreen.kt
@@ -1,8 +1,11 @@
 package com.example.pglocator.Screens
 
-import androidx.compose.foundation.layout.Arrangement
+
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,6 +13,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.pglocator.AuthState
@@ -31,15 +36,20 @@ fun HomeScreen(modifier: Modifier = Modifier,navController: NavController,authVi
 
     Column(
         modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Home Page", fontSize = 32.sp)
 
-        TextButton(onClick = {
-            authViewModel.signout()
-        }) {
-            Text(text = "Sign out")
+    ) {
+        Text(text = "Home Page", fontSize = 32.sp, modifier = Modifier
+            .align(Alignment.CenterHorizontally)
+            .padding(top = 40.dp))
+        Box(modifier = Modifier.wrapContentSize() ) {
+            TextButton(
+                onClick = {
+                    authViewModel.signout()
+                },
+                modifier = Modifier.align(Alignment.BottomEnd)
+            ) {
+                Text(text = "Sign out",)
+            }
         }
 
     }
